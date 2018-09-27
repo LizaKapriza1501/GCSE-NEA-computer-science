0fde16e1c815a042546f2ab8ddf4c9593f021696
LizaKapriza1501 <Liza.Avezbakiev@gmail.com>
2018-09-27 22:53:11 +0100

    some code tests in PY and project report is added

--- task1.py
+++ task1.py
@@ -12,6 +12,29 @@ with open('songs.csv') as csvfile:
         Songs.append(songs)
         Artist.append(artist)
 
+'''
+print("These are songs:\n")
+for song in Songs:
+    print(song)
+
+print("These are artists:\n")
+for artist in Artist:
+    print(artist)
+
+'''
+
+print(len(Songs))
+print(len(Artist))
+
+random_song_index = random.randint(0, len(Songs))
+
+print('this is the random index between %(start)d and %(finish)d = %(random_song)d' %
+    { 'start': 0, 'finish': len(Songs), 'random_song': random_song_index})
+
+
+print('This is a song = %(song_name)s and this is the artist = %(artist)s' %
+    { 'song_name': Songs[random_song_index], 'artist': Artist[random_song_index]})
+'''
 count = 0
 while True:
     userName = input("Hello! Welcome to SongGuess! \n\nUsername: ")
@@ -41,4 +64,4 @@ input = Songs[x]
     words = input.split()
     letters = [word[0] for word in words]
     print("".join(letters))
-
+'''
