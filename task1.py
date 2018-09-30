f7a328b8163a810ed3ebd559ccbae025d543e577
LizaKapriza1501 <Liza.Avezbakiev@gmail.com>
2018-09-30 22:38:42 +0100

    misc changes

--- task1.py
+++ task1.py
@@ -1,6 +1,7 @@
 import csv
 import time
 import random
+
 with open('songs.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter = ',')
     Songs = []
@@ -12,6 +13,16 @@ with open('songs.csv') as csvfile:
         Songs.append(songs)
         Artist.append(artist)
 
+'''
+print("These are songs:\n")
+for song in Songs:
+    print(song)
+
+print("These are artists:\n")
+for artist in Artist:
+    print(artist)
+
+'''
 
 print(len(Songs))
 print(len(Artist))
@@ -40,4 +51,18 @@ while True:
         else:
             print("Your username or password is incorrect, please try again")
 
+guess = 0
+while guess < 3:
+    x = random.randint(0,108)
+    print(Artist[x])
+    title = Songs[x]
+
+    test = title[0] + title[1:].replace(title[0], "_")
+    guess += 1
+
+
+input = Songs[x]
+    words = input.split()
+    letters = [word[0] for word in words]
+    print("".join(letters))
 '''
--- usersPY.py
+++ usersPY.py
@@ -1,16 +1,46 @@
+
 import csv
 import time
 
-with open('users.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter = ',')
-    UserID = []
-    Password = []
-    Status = []
+UserID = []
+Password = []
+Status = []
 
-    for row in readCSV:
-        userID = row[0]
-        password = row[1]
-        status = row[2]
-        UserID.append(userID)
-        Password.append(password)
-        Status.append(status)
+
+def init_users():
+    with open('users.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter = ',')
+        for row in readCSV:
+            userID = row[0]
+            password = row[1]
+            status = row[2]
+            UserID.append(userID)
+            Password.append(password)
+            Status.append(status)
+
+
+
+'''
+find user with the name that is send in user_id_param in the UserID array
+'''
+def userExists(user_id_param):
+    '''
+    liza - change this function, so it is using the findUser function and does NOT do it's own traversing of array
+    '''
+
+    user_found = False
+    for user in UserID:
+        if user == user_id_param:
+            user_found = True
+
+    return user_found
+
+def findUser(user_id_param):
+
+    found_user_index = -1
+    for idx, user in enumerate(UserID):
+        if user == user_id_param:
+            found_user_index = idx
+
+    return found_user_index
+
