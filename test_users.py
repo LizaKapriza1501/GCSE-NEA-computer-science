e54379a30e7338ae794c26a6254b616622670a30
LizaKapriza1501 <Liza.Avezbakiev@gmail.com>
2018-09-30 22:37:56 +0100

    adding .gitignore and test_users.py

---
+++ test_users.py
@@ -0,0 +1,29 @@
+import usersPY
+
+'''
+initialise users
+'''
+
+usersPY.init_users()
+
+'''
+check if user 'user1' exists and print the index for the userID
+'''
+
+user_checker = usersPY.userExists('user1')
+
+print(user_checker)
+
+user_checker = usersPY.userExists('user111')
+
+print(user_checker)
+
+user_index = usersPY.findUser('user5')
+print(user_index)
+
+user_index = usersPY.findUser('user555')
+print(user_index)
+
+
+
+
