df514aa16723649bab3b773de7912a7f37562a5b
Liza Avezbakiev <liza.avezbakiev@gmail.com>
2018-10-03 10:22:41 +0100

    added if statement to check the User Input

--- task1.py
+++ task1.py
@@ -13,19 +13,26 @@ with open('songs.csv') as csvfile:
         Songs.append(songs)
         Artist.append(artist)
 
+def GivenData():
+    GivenUserID = input("Please input your username: ")
+    GivenPassword = input("Please input your password: ")
+    for input in GivenPassword:
+        if GivenUserID == usersPY.userExists():
+            print("your Username is correct")
+        else:
+            print("incorrect Username")
+
+    for input in GivenPassword:
+        if GivenPassword == userPY.passExists():
+            print("your Password is correct")
+        else:
+            print("incorrect Password")
+
+
 '''
-print("These are songs:\n")
-for song in Songs:
-    print(song)
-
-print("These are artists:\n")
-for artist in Artist:
-    print(artist)
-
-'''
-
 print(len(Songs))
 print(len(Artist))
+
 
 random_song_index = random.randint(0, len(Songs))
 
@@ -35,34 +42,5 @@ print('this is the random index between %(start)d and %(finish)d = %(random_song
 
 print('This is a song = %(song_name)s and this is the artist = %(artist)s' %
     { 'song_name': Songs[random_song_index], 'artist': Artist[random_song_index]})
-'''
-count = 0
-while True:
-    userName = input("Hello! Welcome to SongGuess! \n\nUsername: ")
-    password = input("Password: ")
-    count += 1
-    if count == 3:
-        print("GoodBye, you used all of your attempts")
-        break
-    else:
-        if userName == 'Hello' and password == 'World':
-            print("Welcome to SongGuess!")
-            break
-        else:
-            print("Your username or password is incorrect, please try again")
 
-guess = 0
-while guess < 3:
-    x = random.randint(0,108)
-    print(Artist[x])
-    title = Songs[x]
-
-    test = title[0] + title[1:].replace(title[0], "_")
-    guess += 1
-
-
-input = Songs[x]
-    words = input.split()
-    letters = [word[0] for word in words]
-    print("".join(letters))
 '''
